0a26251fa736f72211e1fd374e2d42401d0de867
Hugh Owens <hughrowens14@hotmail.co.uk>
2017-02-09 12:08:47 +0000

    Update TicketType.cs

--- Project/Models/TicketType.cs
+++ Project/Models/TicketType.cs
@@ -7,5 +7,18 @@ namespace Project.Models
 {
     public class TicketType
     {
+        [Key]
+        public int TicketTypeId{get; set;}
+        
+        [Required, StringLength(15)]
+        public string Name {get; set;}
+        
+        [StringLength(100)]
+        public string Description{get; set;}
+        
+        [Required]
+        private double OffPeakPrice  {get, 
+        
+        
     }
 }
